e34fd3382644a55dadcd2bb272528c5f00ffe189
Akshay-Gole <akshaygole01111@gmail.com>
2023-01-26 14:57:51 +0530

    Concept of props and destructuring them

--- pdas/src/App.js
+++ pdas/src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import "bulma/css/bulma.css";
+import ProfileCard from './ProfileCard';
+import AlexaImage from "./images/alexa.png";
+import CortanaImage from './images/cortana.png';
+import SiriImage from "./images/siri.png";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <section className="hero is-primary">
+                <div className="hero-body">
+                    <p className="title">
+                        Personal Digital Assistants
+                    </p>
+                </div>
+            </section>
+
+            <div className="container">
+                <section className="section">
+                    <div className="columns">
+                        <div className="column is-4">
+                            <ProfileCard
+                                title="Alexa"
+                                handle="@alexa99"
+                                image={AlexaImage}
+                                description="This is a description of this profile cards"
+                            />
+                        </div>
+                        <div className="column is-4">
+                            <ProfileCard
+                                title="Cortana"
+                                handle="@cortana32"
+                                image={CortanaImage}
+                                description="This is a description of this profile cards"
+                            />
+                        </div>
+                        <div className="column is-4">
+                            <ProfileCard
+                                title="Siri"
+                                handle="@siri01"
+                                image={SiriImage}
+                                description="This is a description of this profile cards"
+                            />
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </div>
+    );
 }
 
-export default App;
+export default App
